3528123e7fdb69f02e7f611babdc4418ef658206
Ali Rıza SAYDAN <39958391+RootIpV6@users.noreply.github.com>
2024-09-18 19:36:15 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
+
 /export file=config;
 /system backup save name=config;
 /tool e-mail send to="BACKUP'ın GÖNDERİLECEĞİ MAİL ADRESİNİ YAZINIZ" subject=([/system identity get name]." Backup") body=("Mikrotik Configuration Backup for - ".[/system clock get date]) file=config.rsc,config.backup;
+/log info "Backup e-mail sent.";
+
+
+Mikrotik/AUTOBACKUP SCRİPT
+System/Script section.
+/export file=config;
+/system backup save name=config;
+/tool e-mail send to="THE E-MAIL ADDRESS TO WHICH THE BACKUP WILL BE SENT MUST BE WRITTEN." subject=([/system identity get name]." Backup") body=("Mikrotik Configuration Backup for - ".[/system clock get date]) file=config.rsc,config.backup;
 /log info "Backup e-mail sent.";
