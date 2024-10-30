254e0164a7e6610ca4a4775c3d05ddbc9cc3bd0f
Ali Rıza SAYDAN <39958391+RootIpV6@users.noreply.github.com>
2024-10-30 09:48:59 +0300

    v3

---
+++ Winbox-on-Ubuntu-Centos-Debian/Readme.md
@@ -0,0 +1,5 @@
+# Hello Friends
+
+# Installing Winbox for Ubuntu ,Centos and debian distribution
+
+             ALİ RIZA SAYDAN
---
+++ Winbox-on-Ubuntu-Centos-Debian/winbox-to-debian.sh
@@ -0,0 +1,8 @@
+dpkg --add-architecture i386 && apt-get update
+
+# debian repo
+
+echo "deb http://repo.vitexsoftware.com $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/vitexsoftware.list
+sudo wget -O /etc/apt/trusted.gpg.d/vitexsoftware.gpg http://repo.vitexsoftware.com/keyring.gpg
+sudo apt update
+sudo apt install winbox
